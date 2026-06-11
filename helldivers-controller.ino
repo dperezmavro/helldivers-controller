77a0596b985ceb875a69e7aaa6211bbaa819413e
dperezmavro <dio.pm@live.com>
2026-06-11 22:10:23 +0200

    split into files

--- helldivers-controller.ino
+++ helldivers-controller.ino
@@ -3,6 +3,8 @@
 #include <Mouse.h>
 #include <ezButton.h>
 
+#include "stratagem-assignments.h"
+
 // #define DEBUG
 
 #ifdef DEBUG
@@ -26,22 +28,6 @@ constexpr unsigned long WAIT_BETWEEN_KEY_PRESS_MS   = 75;
 constexpr unsigned long DEBOUNCE_TIME_MS            = 50;
 constexpr unsigned long LED_FEEDBACK_MS             = 50;
 constexpr unsigned long MULTI_TRIGGER_PREVENTION_MS = 300;
-
-// Slot 1
-constexpr char REINFORCE_CODE[] = "wsdaw";
-constexpr char STALWART_CODE[]  = "saswwa";
-
-// Slot 2
-constexpr char RESUPPLY_CODE[]        = "sswd";
-constexpr char GUARD_DOG_ROVER_CODE[] = "swawds";
-
-// Slot 3
-constexpr char SENTRY_AUTOCANNON_CODE[] = "swdwaw";
-constexpr char MACHINE_GUN_CODE[]       = "saswd";
-
-// Slot 4
-constexpr char EAGLE_NAPALM_AIRSTRIKE_CODE[]    = "ddsadw";
-constexpr char FIVE_HUNDRED_KG_AIRSTRIKE_CODE[] = "wdsss";
 
 ezButton slot1(PIN_SLOT_1_BTN, INPUT_PULLUP);
 ezButton slot2(PIN_SLOT_2_BTN, INPUT_PULLUP);
---
+++ stratagem-assignments.h
@@ -0,0 +1,21 @@
+#ifndef _STRATAGEM_ASSIGNMENTS
+
+#define _STRATAGEM_ASSIGNMENTS
+
+// Slot 1
+constexpr char REINFORCE_CODE[] = "wsdaw";
+constexpr char STALWART_CODE[]  = "saswwa";
+
+// Slot 2
+constexpr char RESUPPLY_CODE[]        = "sswd";
+constexpr char GUARD_DOG_ROVER_CODE[] = "swawds";
+
+// Slot 3
+constexpr char SENTRY_AUTOCANNON_CODE[] = "swdwaw";
+constexpr char MACHINE_GUN_CODE[]       = "saswd";
+
+// Slot 4
+constexpr char EAGLE_NAPALM_AIRSTRIKE_CODE[]    = "ddsadw";
+constexpr char FIVE_HUNDRED_KG_AIRSTRIKE_CODE[] = "wdsss";
+
+#endif
